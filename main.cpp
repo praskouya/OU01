58bd0b74e8c6fdb15a23f188065f7e9b78cb5749
kozaneckiwojtek <kozaneckiwojtek@gmail.com>
2022-11-03 17:25:35 +0100

    zmiana w metodzie metody policz

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int policz (int a, int b){
-    return a-b;
+    return (a+b)*(a-b);
 }
 int main() {
     using namespace std;
     cout << "Hello, World!" << endl;
-    cout << "W pierwszym branchu; 2-3=" << policz(2,3) << endl;
+    cout << "W pierwszym branchu; 2+3=" << policz(2,3) << endl;
     cout << "slay" << endl;
     return 0;
 }
