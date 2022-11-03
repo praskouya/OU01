5695b32a2b63c1ac38b6a6f1d22c2db1258abcbb
kozaneckiwojtek <kozaneckiwojtek@gmail.com>
2022-11-03 17:17:52 +0100

    zmiana mnozenia w metodzie metody policz

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 int policz (int a, int b){
-    return a+b;
+    return a*b;
 }
 int main() {
     using namespace std;
     cout << "Hello, World!" << endl;
     cout << "W pierwszym branchu; 2+3=" << policz(2,3) << endl;
+    cout << "slay" << endl;
     return 0;
 }
+
